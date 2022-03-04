f9a5d45fbd96c844cd5d415a71c291b80908943f
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2022-03-04 19:33:49 +0700

    fix: fix lint nodejs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
-  extends: ['airbnb', 'prettier', 'plugin:node/recommended'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
-    'no-unused-vars': 'warn',
-    'no-console': 'off',
-    'func-names': 'off',
-    'no-process-exit': 'off',
-    'object-shorthand': 'off',
-    'class-methods-use-this': 'off',
   },
 };
